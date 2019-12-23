8fa6649ace6b0cb4a0db667665bfd769f8288bc9
Smog <smog@itgalaxy.net>
2019-12-23 10:25:42 -0800

    79

--- DatingApp/Helpers/Extensions.cs
+++ DatingApp/Helpers/Extensions.cs
@@ -16,5 +16,12 @@ namespace DatingApp.Helpers
            
            
         }
+
+        public static int CalcalateAge(this DateTime theDateTime)
+        {
+            var age = DateTime.Today.Year - theDateTime.Year;
+            if (theDateTime.AddYears(age) > DateTime.Today) age--;
+            return age;
+        }
     }
 }
